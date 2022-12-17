0427bc38e3357aea4ea95e98967d8070ef5f47cd
Nermin <sehicnermin97@gmail.com>
2022-12-17 20:01:05 +1100

    Refactor NotFoundException

--- src/main/java/me/nerminsehic/exception/NotFoundException.java
+++ src/main/java/me/nerminsehic/exception/NotFoundException.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(Class<?> entity, Object identifier) {
+        super("A %s could not be found using the following identifier: %s".formatted(entity.getName(), identifier));
+    }
+
     public NotFoundException(String message) {
         super(message);
     }
